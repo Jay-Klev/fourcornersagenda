d2045841ed1fdca5d5b059f2291ea445b9a0ea6b
Jay-Klev <46989595+Jay-Klev@users.noreply.github.com>
2020-08-06 22:46:23 -0700

    set static folder

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const app = express();
 const port = 3000;
 const fs = require("fs");
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "ui/html", "index.html"));
-});
+app.use(express.static(path.join(__dirname, "ui")));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "ui/html", "index.html"));
+// });
 
 app.listen(port, () =>
   console.log("example app listening on port ", +port, " .")
