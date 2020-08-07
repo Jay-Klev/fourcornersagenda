8c1e5fafa6bd1cb1dd3a46d6641166cb1cbf695c
Jay-Klev <46989595+Jay-Klev@users.noreply.github.com>
2020-08-06 23:14:48 -0700

    edit styles pages HTML

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 const fs = require("fs");
 
-app.use(express.static(path.join(__dirname, "ui")));
+app.use(express.static(path.join(__dirname, "ui/html")));
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "ui/html", "index.html"));
