ec434fce99e387b17c8db1efe84fc9203575ac7c
Jay-Klev <46989595+Jay-Klev@users.noreply.github.com>
2020-08-07 22:31:42 -0700

    created/tracked members.js modified app.js

---
+++ ui/members.js
@@ -0,0 +1,44 @@
+const members = [
+  {
+    id: 1,
+    name: "Justin Klevgaard",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Patashi",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "Matt",
+    status: "active",
+  },
+  {
+    id: 4,
+    name: "Danica",
+    status: "active",
+  },
+  {
+    id: 5,
+    name: "Angel",
+    status: "active",
+  },
+  {
+    id: 6,
+    name: "Azia",
+    status: "active",
+  },
+  {
+    id: 7,
+    name: "Quintin",
+    status: "active",
+  },
+  {
+    id: 8,
+    name: "Vanessa",
+    status: "active",
+  },
+];
+
+module.exports = members;
