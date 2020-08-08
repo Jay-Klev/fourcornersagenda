aadb81712ab4cf345afc351b7d4bfa31415c7454
Jay-Klev <46989595+Jay-Klev@users.noreply.github.com>
2020-08-07 22:01:12 -0700

    edit app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const fs = require("fs");
 
 app.use(express.static(path.join(__dirname, "ui")));
@@ -11,6 +11,4 @@ app.use(express.static(path.join(__dirname, "ui")));
 //   res.sendFile(path.join(__dirname, "ui/html", "index.html"));
 // });
 
-app.listen(port, () =>
-  console.log("example app listening on port ", +port, " .")
-);
+app.listen(port, () => console.log("example app listening on port ${port}."));
